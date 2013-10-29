0c8c5708ed6e608ff1effd15f4e0734b621ede24
Collen Jones <collenjones@gmail.com>
2013-10-29 12:31:47 -0700

    Basic Node server is up and running serving a GET request

---
+++ request_handler.js
@@ -0,0 +1,29 @@
+var headers = {
+  "access-control-allow-origin": "*",
+  "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "access-control-allow-headers": "content-type, accept",
+  "access-control-max-age": 10, // Seconds.
+  "Content-Type": "application/json"
+};
+
+exports.sendResponse = sendResponse = function(response, obj, status){
+  status = status || 200;
+  response.writeHead(status, headers);
+  response.end(JSON.stringify(obj));
+};
+
+var collectData = function(request, callback){
+  var data = "";
+  request.on('data', function(chunk){
+    data += chunk;
+  });
+  request.on('end', function(){
+    callback(data);
+  });
+};
+
+exports.eventHandler = function(req, res) {
+  console.log("Serving request type " + req.method + " for url " + req.url);
+  res.writeHead(200, headers);
+  res.end("You got this!");
+};
--- scraper.js
+++ scraper.js
@@ -1,8 +1,12 @@
-var http = require('http');
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+var requestHandler = require("./request_handler.js");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+var port = 8080;
 
-console.log('Server running at http://127.0.0.1:1337/');
+var ip = "127.0.0.1";
+
+var server = http.createServer(requestHandler.eventHandler);
+console.log("Listening on http://" + ip + ":" + port);
+server.listen(port, ip);
