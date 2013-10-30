e4acbcfe7f9e5d39dfe9ad7c614bdfe3c3e9f45c
Collen Jones <collenjones@gmail.com>
2013-10-29 23:51:58 -0700

    Can scrape twitter server for tweets

--- server/request_handler.js
+++ server/request_handler.js
@@ -1,9 +1,6 @@
-var mysql = require('mysql');
 var url = require('url');
 var util = require('util');
-var twitter = require('twitter');
-var moment = require('moment-timezone');
-var api = require('../api.config');
+
 
 // Define CORS headers
 var headers = {
@@ -13,20 +10,6 @@ var headers = {
   "access-control-max-age": 10, // Seconds
   "Content-Type": "application/json"
 };
-
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'little_bird',
-  database : 'little_bird'
-});
-
-// Connect to Twitter API
-var twit = new twitter({
-  consumer_key: api.twitter.key,
-  consumer_secret: api.twitter.secret,
-  access_token_key: api.twitter.access_token,
-  access_token_secret: api.twitter.access_token_secret
-});
 
 exports.sendResponse = sendResponse = function(response, obj, status){
   status = status || 200;
@@ -56,27 +39,6 @@ exports.eventHandler = function(req, res) {
           res.end("GET method received");
           break;
         case '/seed-tweets':
-          twit.search('bitcoin OR #bitcoin', function(data) {
-            for(var i = 0; i < data.statuses.length; i++) {
-              var user = data.statuses[i].user.screen_name;
-              var text = data.statuses[i].text;
-              // convert timezone to San Francisco time
-              var timestamp = moment(data.statuses[i].created_at).tz("America/Los_Angeles").format('YYYY-MM-DD HH:MM:SS');
-              connection.query("INSERT INTO Tweets (username, text, timestamp) VALUES (?, ?, ?)", 
-                [user, text, timestamp], 
-                function(err, rows, fields) {
-                  if (err) {
-                    console.log(err);
-                  }
-                // connection.end();
-              });
-            }
-            // get hashtag here
-            // need to convert timestamp to the proper timezone
-            
-            
-          });
-          
           res.writeHead(200, headers);
           res.end("time to seed some tweets!");
           break;
--- server/seed-server.js
+++ server/seed-server.js
@@ -1,6 +1,25 @@
 var http = require("http");
-
+var cronJob = require('cron').CronJob;
+var twitter = require('twitter');
+var moment = require('moment-timezone');
+var mysql = require('mysql');
+var api = require('../api.config');
 var requestHandler = require("./request_handler.js");
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'little_bird',
+  database : 'little_bird',
+  multipleStatements: true
+});
+
+// Connect to Twitter API
+var twit = new twitter({
+  consumer_key: api.twitter.key,
+  consumer_secret: api.twitter.secret,
+  access_token_key: api.twitter.access_token,
+  access_token_secret: api.twitter.access_token_secret
+});
 
 var port = 8080;
 var ip = "127.0.0.1";
@@ -8,3 +27,32 @@ var ip = "127.0.0.1";
 var server = http.createServer(requestHandler.eventHandler);
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
+
+var scrapeTweets = function() {
+  twit.search('bitcoin', {count: 100}, function(data) {
+    for(var i = 0; i < data.statuses.length; i++) {
+      var username = data.statuses[i].user.screen_name;
+      var text = data.statuses[i].text;
+      // convert timezone to San Francisco time
+      var timestamp = moment(data.statuses[i].created_at).tz("America/Los_Angeles").format('YYYY-MM-DD HH:MM:SS');
+      connection.query("INSERT INTO Tweets (username, text, timestamp) VALUES (?, ?, ?)", 
+        [username, text, timestamp], 
+        function(err, rows, fields) {
+          if (err) {
+            console.log(err);
+          }
+        // connection.end();
+      });
+    }
+    // get hashtag here
+    // need to convert timestamp to the proper timezone  
+  });
+};
+
+var job = new cronJob('06 * * * * *', function(){
+    scrapeTweets();
+  }, function () {
+    console.log('Cache updated');
+  },
+  true /* Start the job right now */
+);
