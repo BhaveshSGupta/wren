4f22fab4dc27141d972a7db9d8f1adbab08f3935
Collen Jones <collenjones@gmail.com>
2013-10-30 19:11:33 -0700

    Possible correction of sporadic unhandled tweet text when inserting into MySQL database

--- server/seed-server.js
+++ server/seed-server.js
@@ -2,16 +2,17 @@ var http = require("http");
 var twitter = require('twitter');
 var moment = require('moment-timezone');
 var mysql = require('mysql');
-var JAVA = require('java');
-var StanfordSimpleNLP = require('stanford-simple-nlp').StanfordSimpleNLP;
+var mtgox = require('mtgox');
+var Topsy = require('node-topsy');
 var api = require('../api.config');
 var requestHandler = require("./request_handler.js");
 
+// establish database connection
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'little_bird',
   database : 'little_bird',
-  charset  : 'utf-8',
+  charset  : 'utf-32',
   multipleStatements: true
 });
 
@@ -23,6 +24,16 @@ var twit = new twitter({
   access_token_secret: api.twitter.access_token_secret
 });
 
+// connect to MtGox service
+var MtGox = require('mtgox');
+var gox = new MtGox();
+// Key+secret is required to access the private API
+gox = new MtGox({
+    key: api.mtgox.key,
+    secret: api.mtgox.secret
+});
+
+
 var port = 8080;
 var ip = "127.0.0.1";
 
@@ -31,7 +42,7 @@ console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
 
 var scrapeTweets = function() {
-  twit.search('bitcoin', {count: 100}, function(data) {
+  twit.search('bitcoin OR bitcoins', {lang: 'en', count: 100}, function(data) {
     if(data.statuses){
       for(var i = 0; i < data.statuses.length; i++) {
         // closure function to correctly pass 'i' into the callbacks
@@ -39,7 +50,7 @@ var scrapeTweets = function() {
           var username = data.statuses[i].user.screen_name;
           var text = data.statuses[i].text;
           // convert timezone to San Francisco time
-          var timestamp = moment(data.statuses[i].created_at).tz("America/Los_Angeles").format('YYYY-MM-DD HH:MM:SS');
+          var timestamp = moment(data.statuses[i].created_at).tz("America/Los_Angeles").format('YYYY-MM-DD hh:mm:ss');
 
           // check if tweet already exists
           connection.query("SELECT 1 FROM Tweets WHERE username=? AND timestamp=?", [username, timestamp],
@@ -70,5 +81,46 @@ var scrapeTweets = function() {
   });
 };
 
+// get MtGox order depth
+var scrapeMtGox = function(){
+  gox.market('BTCUSD', function(err, depth) {
+    if(depth) {
+      var site = 1; // mtgox value in table
+      //var timestamp = moment(Math.floor(depth.timestamp / 1000)).tz("America/Los_Angeles").format('YYYY-MM-DD HH:MM:SS'); // convert timestamp to database format
+      var timestamp = new Date(depth.timestamp/1000);
+      timestamp = moment(timestamp).format('YYYY-MM-DD hh:mm:ss');
+      var volume = depth.volume;
+      var value = depth.average;
+      // need to convert timestamp
+      connection.query("SELECT 1 FROM MarketMovement WHERE timestamp=?", [timestamp],
+        function(err, rows, fields){
+          if(err) {
+            console.log(err);
+            return;
+          }
+
+          // insert into database if doesn't already exist
+          if(rows.length === 0) {
+            connection.query("INSERT INTO MarketMovement (site, timestamp, volume, value) VALUES (?, ?, ?, ?)", 
+              [site, timestamp, volume, value], 
+              function(err, rows, fields) {
+                if (err) {
+                  console.log(err);
+                }
+              }
+            );
+          }
+        }
+      );
+    }
+  });
+}
+
+// var topsy = new Topsy('YFDRGIFTRJN23G4LG1X2MNEWSKDAL2CU');
+// topsy.getSearch({"q": "bitcoins", limit: 20000}, function(error, result) {
+//     console.log(result);
+// });
+
 // Using setInterval() for scraping since cronJob cannot schedule by the second
-setInterval(scrapeTweets, 6000);
+setInterval(scrapeTweets, 6000);  // Twitter API Rate Limit is 180 requests per 15 min
+setInterval(scrapeMtGox, 30500);  // MtGox API Rate Limit is once per 30s
