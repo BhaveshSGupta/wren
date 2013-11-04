699015747010d90ba835412445389e050210ebbf
Collen Jones <collenjones@gmail.com>
2013-11-03 20:49:41 -0800

    JSLint sentiment.js

--- server/helpers/sentiment.js
+++ server/helpers/sentiment.js
@@ -13,26 +13,27 @@ var connection = mysql.createConnection({
   multipleStatements: true
 });
 
-exports.recalcSentiment = function(){
-  connection.query("SELECT * FROM tweets", 
-    function(err, rows, fields){
-      if(err){
+exports.recalcSentiment = function () {
+  connection.query("SELECT * FROM tweets",
+    function (err, rows, fields) {
+      if (err) {
         console.log(err);
         return;
       }
-      var closureFunc = function(i){
-        var scrubbedText = rows[i].text.replace(/(\bhtt)\S+\b/g,'');  // strip out sites for analysis (they return neutral)
+      var i;
+      var closureFunc = function (i) {
+        var scrubbedText = rows[i].text.replace(/(\bhtt)\S+\b/g, '');  // strip out sites for analysis (they return neutral)
         var tweet_sentiment = analyze(scrubbedText).score;
         connection.query("UPDATE tweets SET sentiment=? WHERE id=?",
           [tweet_sentiment, rows[i].id],
-          function(err, rows, fields){
-            if(err){
+          function (err, rows, fields) {
+            if (err) {
               console.log(err);
               return;
             }
           });
       };
-      for(var i = 0; i < rows.length; i++){
+      for (i = 0; i < rows.length; i++) {
         closureFunc(i);
       }
       console.log('DONE CALCULATING SENTIMENT FOR ALL TWEETS!!!');
@@ -68,49 +69,50 @@ exports.recalcSentiment = function(){
 function getSentiment(error, stdout, stderr) {
   var output = stderr.split('\n');
   var sentimentArray = [];
-  for(var i = 0; i < output.length; i++){
-    switch(output[i]){
-      case "  Predicted sentiment: Very positive":
-        sentimentArray.push(4);
-        break;
-      case "  Predicted sentiment: Positive":
-        sentimentArray.push(3);
-        break;
-      case "  Predicted sentiment: Neutral":
-        sentimentArray.push(2);
-        break;
-      case "  Predicted sentiment: Negative":
-        sentimentArray.push(1);
-        break;
-      case "  Predicted sentiment: Very negative":
-        sentimentArray.push(0);
-        break;
-      default:
-        break;
+  var i;
+  for (i = 0; i < output.length; i++) {
+    switch (output[i]) {
+    case "  Predicted sentiment: Very positive":
+      sentimentArray.push(4);
+      break;
+    case "  Predicted sentiment: Positive":
+      sentimentArray.push(3);
+      break;
+    case "  Predicted sentiment: Neutral":
+      sentimentArray.push(2);
+      break;
+    case "  Predicted sentiment: Negative":
+      sentimentArray.push(1);
+      break;
+    case "  Predicted sentiment: Very negative":
+      sentimentArray.push(0);
+      break;
+    default:
+      break;
     }
   }
- 
+
   var totSentiment = 0;
-  for(var i = 0; i < sentimentArray.length; i++){
+  for (i = 0; i < sentimentArray.length; i++) {
     totSentiment += sentimentArray[i];
   }
-  return Math.round(totSentiment/sentimentArray.length);
+  return Math.round(totSentiment / sentimentArray.length);
 }
 
-var runDeeplyMoving = function(filepath, callback){
+var runDeeplyMoving = function (filepath, callback) {
   exec('java -cp "deeply_moving/*" -mx5g edu.stanford.nlp.sentiment.SentimentPipeline -file ' + filepath, callback);
 };
 
-exports.calcSentiment = function(tweet_id, text){
+exports.calcSentiment = function (tweet_id, text) {
   var sentiment = 0;
-  var scrubbedText = text.replace(/(\bhtt)\S+\b/g,'');  // strip out sites for analysis (they return neutral)
+  var scrubbedText = text.replace(/(\bhtt)\S+\b/g, '');  // strip out sites for analysis (they return neutral)
   var filepath = "db/data/sentiment/" + tweet_id + ".snt";
   fs.writeFileSync(filepath, scrubbedText + '\n');
-  runDeeplyMoving(filepath, function(err, stdout, stderr) {
+  runDeeplyMoving(filepath, function (err, stdout, stderr) {
     sentiment = getSentiment(err, stdout, stderr);
     // delete the file
-    fs.unlink(filepath, function(err) {
-      if(err){
+    fs.unlink(filepath, function (err) {
+      if (err) {
         console.log(err);
         return;
       }
@@ -120,41 +122,40 @@ exports.calcSentiment = function(tweet_id, text){
   return sentiment;
 };
 
-exports.storeSentiment = function(tweet_id){
+exports.storeSentiment = function (tweet_id) {
   connection.query("SELECT * FROM tweets WHERE sentiment IS NULL",
-    function(err, rows, fields) {
-      if(err) {
+    function (err, rows, fields) {
+      if (err) {
         console.log(err);
         return;
       }
-      var closureFunc = function(i, filepath) {
-        var text = rows[i].text.replace(/(\bhtt)\S+\b/g,'');  // strip out sites for analysis (they return neutral)
+      var closureFunc = function (i, filepath) {
+        var text = rows[i].text.replace(/(\bhtt)\S+\b/g, '');  // strip out sites for analysis (they return neutral)
         fs.writeFileSync(filepath, text + '\n');
-        runDeeplyMoving(filepath, function(err, stdout, stderr) {
+        runDeeplyMoving(filepath, function (err, stdout, stderr) {
           var sentiment = getSentiment(err, stdout, stderr);
           // insert sentiment into tweets_copy
-          connection.query("UPDATE tweets SET sentiment=? WHERE id=?", 
-            [sentiment, rows[i].id], 
-            function(err, rows, fields) {
-              if(err) {
+          connection.query("UPDATE tweets SET sentiment=? WHERE id=?",
+            [sentiment, rows[i].id],
+            function (err, rows, fields) {
+              if (err) {
                 console.log(err);
                 return;
               }
-            }
-          );
+            });
           // delete the file
-          fs.unlink(filepath, function(err) {
-            if(err){
+          fs.unlink(filepath, function (err) {
+            if (err) {
               console.log(err);
               return;
             }
           });
         });
       };
-      
-      var processChunk = function(i) {
+
+      var processChunk = function (i) {
         var end = Math.min(i + 10, rows.length);
-        for(; i < end; i++){
+        for (;i < end; i++) {
           var filepath = "db/data/sentiment/" + rows[i].tweet_id + ".snt";
           closureFunc(i, filepath);
         }
