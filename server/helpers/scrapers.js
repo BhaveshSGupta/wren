b16f81c5acc8ca2fd0970513449e72bc51b6e8c2
Collen Jones <collenjones@gmail.com>
2013-11-03 20:37:20 -0800

    JSLint scrapers.js

--- server/helpers/scrapers.js
+++ server/helpers/scrapers.js
@@ -30,50 +30,50 @@ var MtGox = require('mtgox');
 var gox = new MtGox();
 // Key+secret is required to access the private API
 gox = new MtGox({
-    key: api.mtgox.key,
-    secret: api.mtgox.secret
+  key: api.mtgox.key,
+  secret: api.mtgox.secret
 });
 
 // TODO: Refactor the scraper to use the same function call for different services
-exports.scrapeTweets = function() {
-  twit.search('bitcoin OR bitcoins OR #mtgox OR #bitstamp OR #btce', {lang: 'en', count: 100}, function(data) {
-    if(data.statuses){
-      var closureFunc = function(i){
+exports.scrapeTweets = function () {
+  twit.search('bitcoin OR bitcoins OR #mtgox OR #bitstamp OR #btce', {lang: 'en', count: 100}, function (data) {
+    if (data.statuses) {
+      var i;
+      var closureFunc = function (i) {
         var tweet_id = data.statuses[i].id.toString();
         var username = data.statuses[i].user.screen_name;
         var text = data.statuses[i].text;
         // remove non-unicode characters (probably better to whitelist what I will accept rather than create a blacklist)
-        text = text.replace( /([^\x00-\xFF]|\s)*$/g, '' );
+        text = text.replace(/([^\x00-\xFF]|\s)*$/g, '');
         // convert timezone to San Francisco time
-        var timestamp = moment(data.statuses[i].created_at).tz("America/Los_Angeles").format('YYYY-MM-DD HH:mm:ss');     
+        var timestamp = moment(data.statuses[i].created_at).tz("America/Los_Angeles").format('YYYY-MM-DD HH:mm:ss');
 
         // check that tweet does not already exist
         connection.query("SELECT 1 FROM Tweets WHERE tweet_id=?", [tweet_id],
-          function(err, rows, fields) {
-            if(err) {
+          function (err, rows, fields) {
+            if (err) {
               console.log(err);
               return;
             }
 
             // insert into database if doesn't already exist
-            if(rows.length === 0) {
+            if (rows.length === 0) {
               // need to throttle or will choke the system
               // var tweet_sentiment = sentiment.calcSentiment(tweet_id, text);
               var tweet_sentiment = analyze(text).score;
               // console.log('text: ', text, 'sentiment: ', analyze(text).score);
-              connection.query("INSERT INTO Tweets (username, text, timestamp, sentiment, tweet_id) VALUES (?, ?, ?, ?, ?)", 
-                [username, text, timestamp, tweet_sentiment, tweet_id], 
-                function(err, rows, fields) {
+              connection.query("INSERT INTO Tweets (username, text, timestamp, sentiment, tweet_id) VALUES (?, ?, ?, ?, ?)",
+                [username, text, timestamp, tweet_sentiment, tweet_id],
+                function (err, rows, fields) {
                   if (err) {
+                    console.log('text causing error: ', text);
                     console.log(err);
                   }
-                }
-              );
+                });
             }
-          }
-        ); 
+          });
       };
-      for(var i = 0; i < data.statuses.length; i++) {
+      for (i = 0; i < data.statuses.length; i++) {
         // closure function to correctly pass 'i' into the callbacks
         closureFunc(i);
       }
@@ -83,36 +83,34 @@ exports.scrapeTweets = function() {
 };
 
 // get MtGox order depth
-exports.scrapeMtGox = function(){
-  gox.market('BTCUSD', function(err, depth) {
-    if(depth) {
+exports.scrapeMtGox = function () {
+  gox.market('BTCUSD', function (err, depth) {
+    if (depth) {
       var site = 1; // mtgox value in table
       //var timestamp = moment(Math.floor(depth.timestamp / 1000)).tz("America/Los_Angeles").format('YYYY-MM-DD HH:MM:SS'); // convert timestamp to database format
-      var timestamp = new Date(depth.timestamp/1000);
+      var timestamp = new Date(depth.timestamp / 1000);
       timestamp = moment(timestamp).format('YYYY-MM-DD HH:mm:ss');
       var volume = depth.volume;
       var value = depth.average;
       // need to convert timestamp
       connection.query("SELECT 1 FROM MarketMovement WHERE timestamp=?", [timestamp],
-        function(err, rows, fields){
-          if(err) {
+        function (err, rows, fields) {
+          if (err) {
             console.log(err);
             return;
           }
 
           // insert into database if doesn't already exist
-          if(rows.length === 0) {
-            connection.query("INSERT INTO MarketMovement (site, timestamp, volume, value) VALUES (?, ?, ?, ?)", 
-              [site, timestamp, volume, value], 
-              function(err, rows, fields) {
+          if (rows.length === 0) {
+            connection.query("INSERT INTO MarketMovement (site, timestamp, volume, value) VALUES (?, ?, ?, ?)",
+              [site, timestamp, volume, value],
+              function (err, rows, fields) {
                 if (err) {
                   console.log(err);
                 }
-              }
-            );
+              });
           }
-        }
-      );
+        });
     }
   });
-}
+};
