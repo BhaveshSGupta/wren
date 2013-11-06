1f4018dd2725189578e6d6380abdfbba1b31651f
Collen Jones <collenjones@gmail.com>
2013-11-06 15:44:45 -0800

    Remove hardcoded ip from node server

--- server/basic-server.js
+++ server/basic-server.js
@@ -3,11 +3,11 @@ var requestHandler = require("./request_handler.js");
 var scrapers = require("./helpers/scrapers.js");
 
 var port = process.env.PORT || 5000;
-var ip = "127.0.0.1";
+// var ip = "127.0.0.1";
 
 var server = http.createServer(requestHandler.eventHandler);
-console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
+console.log("Listening on port: ", port);
+server.listen(port/*, ip*/);
 
 // Using setInterval() for scraping since cronJob cannot schedule by the second
 setInterval(scrapers.scrapeTweets, 6000);  // Twitter API Rate Limit is 180 requests per 15 min
