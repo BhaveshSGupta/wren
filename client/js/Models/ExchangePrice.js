67038b345d8e703b3ac410bb0ea017627c74cd54
Collen Jones <collenjones@gmail.com>
2014-05-05 18:33:32 -0700

    Convert exchangePrices to USD with fixed conversion

--- client/js/Models/ExchangePrice.js
+++ client/js/Models/ExchangePrice.js
@@ -7,6 +7,11 @@ App.Models.ExchangePrice = Backbone.Model.extend({
   initialize: function(options) {
     _.extend(this, options);
 
+    if(this.get('currency') !== 1) {
+      // Convert currency into USD (Currently on BTC China is not USD)
+      this.set('value', this.get('value') / 6.2);
+    }
+
     this.set('timestamp', this.get('timestamp') * 1000); // convert to ms for HighStocks
   },
 
