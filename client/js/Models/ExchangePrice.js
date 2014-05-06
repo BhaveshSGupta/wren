a8bd2a279eceb5d1ce288f57472ad450ea152fd6
Collen Jones <collenjones@gmail.com>
2014-05-05 19:33:44 -0700

    Refactor data formatting

--- client/js/Models/ExchangePrice.js
+++ client/js/Models/ExchangePrice.js
@@ -7,12 +7,11 @@ App.Models.ExchangePrice = Backbone.Model.extend({
   initialize: function(options) {
     _.extend(this, options);
 
-    if(this.get('currency') !== 1) {
-      // Convert currency into USD (Currently on BTC China is not USD)
-      this.set('value', this.get('value') / 6.2);
-    }
+    this.convertCurrency();
+    this.convertTimestamp();
 
-    this.set('timestamp', this.get('timestamp') * 1000); // convert to ms for HighStocks
+    this.on('change', this.convertCurrency, this);
+    this.on('change', this.convertTimestamp, this);
   },
 
   defaults: {
@@ -20,5 +19,18 @@ App.Models.ExchangePrice = Backbone.Model.extend({
     timestamp: null,
     site: null,
     currency: null
+  },
+
+  convertCurrency: function() {
+    if(this.get('currency') !== 1) {
+      // Convert currency into USD (Currently only BTC China is not USD)
+      this.set('value', this.get('value') / 6.2, {silent: true});
+    }
+  },
+
+  convertTimestamp: function() {
+    if(this.get('timestamp')) {
+      this.set('timestamp', this.get('timestamp') * 1000, {silent: true}); // convert to ms for HighStocks
+    }
   }
 });
