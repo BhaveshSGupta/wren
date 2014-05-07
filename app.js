c27f4a16b2efd500c6d048d2aab3bd07b4c23a99
Collen Jones <collenjones@gmail.com>
2014-05-06 18:32:35 -0700

    Fix footer icon centering

--- app.js
+++ app.js
@@ -12,13 +12,15 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-// Retrieve data from 3rd Party APIs
-// setInterval(function() {
-//   // CryptoCurrency APIs
-//   bitstampAPI.getExchangePrices();  // Bitstamp rate limit is 600 per 10 minutes
-//   btcChinaAPI.getExchangePrices();  // No API Rate Limit is listed
-//   btceAPI.getExchangePrices();      // No API Rate Limit is listed
+if(process.env.NODE_ENV === 'production') {
+  // Retrieve data from 3rd Party APIs
+  setInterval(function() {
+    // CryptoCurrency APIs
+    bitstampAPI.getExchangePrices();  // Bitstamp rate limit is 600 per 10 minutes
+    btcChinaAPI.getExchangePrices();  // No API Rate Limit is listed
+    btceAPI.getExchangePrices();      // No API Rate Limit is listed
 
-//   // Twitter API
-//   twitterAPI.getTweets();           // Twitter API Rate Limit is 180 requests per 15 min
-// }, 60000);
+    // Twitter API
+    twitterAPI.getTweets();           // Twitter API Rate Limit is 180 requests per 15 min
+  }, 60000);
+}
